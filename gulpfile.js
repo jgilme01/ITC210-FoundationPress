bef280134f2345caf6d05404e8089a4bfa4a0d45
jgilme01 <jgilme01@seattlecentral.edu>
2017-08-07 18:11:19 -0700

    new

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var isProduction = !!(argv.production);
 var COMPATIBILITY = [
   'last 2 versions',
   'ie >= 9',
-  'Android >= 2.3'
+  'Android >= 2.3' 
 ];
 
 // File paths to various assets are defined here.
