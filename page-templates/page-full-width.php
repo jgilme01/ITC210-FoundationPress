1cc9e5aa764b37e7318a1780b582dd5e65e1fa02
Gracie <grace@ugraphix.com>
2017-08-07 18:21:17 -0700

    saved work

--- page-templates/page-full-width.php
+++ page-templates/page-full-width.php
@@ -4,16 +4,21 @@ Template Name: Full Width
 */
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
-
+<?php// get_template_part( 'template-parts/featured-image' ); ?>
+<?php $backImg = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );?>
+<header class="entry-title-header" style="background-image: url('<?php echo $backImg[0]; ?>');">
+    <h1 class="entry-title"><?php the_title(); ?></h1>
+</header>
 <div class="main-wrap full-width" role="main">
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+<!--
 		<header>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
+-->
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php the_content(); ?>
