bc807649f481516883ac9ec51d321dd6a5e9a794
Gracie <grace@ugraphix.com>
2017-08-11 16:49:51 -0700

    added accordion menu
    Added accordion menu to community resources. Needs additional tweaks to match visual design.

---
+++ community-resources.php
@@ -0,0 +1,123 @@
+<div class="shelters">
+    <div class="centertext">
+        
+        <h3 id="accordion-menu" class="docs-heading" data-magellan-target="accordion-menu"><a href="#accordion-menu"></a>Shelters</h3>
+						<ul class="vertical accordianmenu" data-accordion-menu>
+							<li>
+							<a href="#">Item 1</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+							
+                            <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+                            
+                             <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+                            
+						</ul>
+					<hr>
+    </div>    
+</div>   
+    
+<div class="foodbanks">
+    <div class="centertext">
+        
+        
+		<h3 id="accordion-menu" class="docs-heading" data-magellan-target="accordion-menu"><a href="#accordion-menu"></a>Food Banks</h3>
+						<ul class="vertical accordianmenu" data-accordion-menu>
+							<li>
+							<a href="#">Item 1</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+							
+                            <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+                            
+                             <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li class="item"><a href="#">Item 1</a></li>
+								<li class="item"><a href="#">Item 2</a></li>
+								<li class="item"><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+                            
+						</ul>
+					<hr>
+    </div>
+</div>
+    
+
+
+    
+<div class="casemanagers">
+    <div class="centertext">
+        
+        
+		<h3 id="accordion-menu" class="docs-heading" data-magellan-target="accordion-menu"><a href="#accordion-menu"></a>Case Managers</h3>
+						<ul class="vertical accordianmenu" data-accordion-menu>
+							<li>
+							<a href="#">Item 1</a>
+							<ul class="accordianmenu vertical nested">
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+							
+                            <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+							
+                            
+                             <li>
+							<a href="#">Item 2</a>
+							<ul class="accordianmenu vertical nested">
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+							</li>
+                            
+						</ul>
+					<hr>
+    </div>
+</div>
+
+
--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?> >
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="" />
 	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
