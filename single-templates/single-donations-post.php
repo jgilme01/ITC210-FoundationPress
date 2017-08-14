804dbda5252c8de69761358031d2efe60d789d7f
Kristinmahyout <kydelv@gmail.com>
2017-08-14 16:45:40 -0700

    event single post

---
+++ single-templates/single-donations-post.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Donation Post
+ * Template Post Type: post
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<?php //get_template_part( 'template-parts/featured-image' ); ?>
+
+<?php $backImg = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );?>
+<header class="entry-title-header" style="background-image: url('<?php echo $backImg[0]; ?>');">
+</header>
+
+<div class="main-wrap" role="main">
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+
+<!--start the loop-->
+<?php while ( have_posts() ) : the_post(); ?>
+	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+<h1 class="entry-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h1>
+<?php foundationpress_entry_meta(); ?>
+		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+		</div>
+		<footer>
+			<?php
+				wp_link_pages(
+					array(
+						'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
+						'after'  => '</p></nav>',
+					)
+				);
+			?>
+			<p><?php the_tags(); ?></p>
+		</footer>
+		<?php the_post_navigation(); ?>
+		<?php do_action( 'foundationpress_post_before_comments' ); ?>
+		<?php comments_template(); ?>
+		<?php do_action( 'foundationpress_post_after_comments' ); ?>
+	</article>
+<?php endwhile;?>
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+<?php get_sidebar(); ?>
+</div>
+<?php get_footer();
