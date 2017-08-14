f82c1790cedfa03bb65892f0f53e1b0ae2812818
jgilme01 <jgilme01@seattlecentral.edu>
2017-08-14 15:34:21 -0700

    working php with advanced custom fields on accordionjs page

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -3,5 +3,8 @@
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
 	<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+		
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		
 	</header>
 <?php endif;
