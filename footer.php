1d934567fe842e8b8d0438d1f8354d15ba8c2703
jae1 <7904001@gmail.com>
2017-08-14 14:41:22 -0700

    separates social and copyright

--- footer.php
+++ footer.php
@@ -10,40 +10,41 @@
 
 ?>
 
-<!--		</div>-->
-
-		<div class="footer-container" data-sticky-footer>
-			<div class="news">
-				<div class="brand">
-					<img src="<?php echo bloginfo('template_url'); ?>/assets/images/logo-black.svg" alt="Homeless Humans of Tacoma">
+	<div class="social-container">
+		<div class="news">
+			<div class="brand">
+				<img src="<?php echo bloginfo('template_url'); ?>/assets/images/logo-black.svg" alt="Homeless Humans of Tacoma">
+			</div>
+			<div class="newsletter">
+				<h4>Sign up to receive updates from us</h4>
+				<?php //if (function_exists('newsletter_form')) newsletter_form(); ?>
+				<div class="">
+					<form method="post" action="//localhost:3000/hhot/?na=s" onsubmit="return newsletter_check(this)">
+						<div>
+							<input class="newsletter-email" type="email" name="ne" placeholder="Your email address" required> <input class="newsletter-submit button small" type="submit" value="Subscribe">
+						</div>
+					</form>
 				</div>
-				<div class="newsletter">
-					<h4>Sign up to receive updates from us</h4>
-					<?php //if (function_exists('newsletter_form')) newsletter_form(); ?>
-					<div class="">
-						<form method="post" action="//localhost:3000/hhot/?na=s" onsubmit="return newsletter_check(this)">
-							<div>
-								<input class="newsletter-email" type="email" name="ne" placeholder="Your email address" required> <input class="newsletter-submit button small" type="submit" value="Subscribe">
-							</div>
-						</form>
-					</div>
-				</div>
-				<div class="social">
-					<h4>Connect with us</h4>
-					<a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-					<a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
-				</div>
-			</div>			
-			
-			<footer class="footer">
-				<?php do_action( 'foundationpress_before_footer' ); ?>
-				<?php dynamic_sidebar( 'footer-widgets' ); ?>
-				<?php do_action( 'foundationpress_after_footer' ); ?>
+			</div>
+			<div class="social">
+				<h4>Connect with us</h4>
+				<a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+			</div>
+		</div>			
+	</div>
+	<div class="footer-container" data-sticky-footer>
+		<footer class="footer">
+			<?php do_action( 'foundationpress_before_footer' ); ?>
+			<?php dynamic_sidebar( 'footer-widgets' ); ?>
+			<?php do_action( 'foundationpress_after_footer' ); ?>
+			<section class="copy">
 				<small>&copy; 2017 Homeless Humans of Tacoma. All Rights Reserved.</small>
-			</footer>
-		</div>
+			</section>
+		</footer>
+	</div>
 
-		<?php do_action( 'foundationpress_layout_end' ); ?>
+	<?php do_action( 'foundationpress_layout_end' ); ?>
 
 <?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		</div><!-- Close off-canvas content -->
