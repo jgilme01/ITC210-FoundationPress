686082d9a9b295fa5399e1cf989ce4232aa84313
jgilme01 <jgilme01@seattlecentral.edu>
2017-08-16 14:24:09 -0700

    accordionjs template zebra

--- page-templates/page-accordionjs.php
+++ page-templates/page-accordionjs.php
@@ -11,7 +11,6 @@ get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-wrap full-width" role="main">
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-	<!--<article <?php //post_class('main-content') ?> id="post-<?php //the_ID(); ?>">-->
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php the_content(); ?>
@@ -20,47 +19,65 @@ get_template_part( 'template-parts/featured-image' ); ?>
 		<?php		
 		$post_ID = get_the_ID();
 		endwhile;?>
-	<!--</article>-->
 	
-<?php /*
-	$posttags = get_the_tags();
-if ($posttags) {
-  foreach($posttags as $tag) {
-    echo $tag->name . ' '; 
-  }
-}
-	*/?>
-
 
 <?php 
 	
-$acf_field_group = new acf_field_group();
-$groups = $acf_field_group->get_field_groups();
+$groups = apply_filters('acf/get_field_groups', array(), array('post_id' => $post_ID));
+
 	
-	//var_dump($groups);
+//var_dump($groups);
 	
+$meta = get_post_meta($post_ID, 'Types');
+//var_dump($meta[0]);
+$counter = 0;
 foreach ($groups as $group){
+	if (strpos($meta[0], $group['title']) !== false){
+	$counter += 1;
 	$group_id = $group['id'];
 	$fields = array();
 	$fields = apply_filters('acf/field_group/get_fields', $fields, $group_id);
-	echo '<div class="row">
-		<div class="small-6 large-6 columns" id="group"><h3 id="group-title">' . $group['title'] . '</h3></div>
+	//echo '<p>' . var_dump($group) . '</p>';
+	if($counter%2!=0){
+	echo '<div class="row" id="zebra">
+		<div class="small-6 large-6 columns" id="group"><h5 id="group-title">' . $group['title'] . '</h5></div>
 		<div class="small-6 large-6 columns">';
 		
 			foreach( $fields as $field ) {
 			$value = get_field( $field['name'] );
 			echo
-		'<ul class="accordion" data-accordion>
+		'<ul class="accordion" data-accordion data-multi-expand="true" data-allow-all-closed="true" id="zebra">
   		<li class="accordion-item" data-accordion-item>
     	<a href="#" class="accordion-title">' . $field['label'] . '</a>
-    	<div class="accordion-content" data-tab-content>
-      	<div id="map">' . $value . '</div>
+    	<div class="accordion-content" data-tab-content>' . $value . '
     		</div>
   			</li>
 			</ul>';}
 			echo
 			'</div>
 			</div>';}
+	elseif($counter%2==0){
+		echo '<div class="row">
+		<div class="small-6 large-6 columns" id="group"><h5 id="group-title">' . $group['title'] . '</h5></div>
+		<div class="small-6 large-6 columns">';
+		
+			foreach( $fields as $field ) {
+			$value = get_field( $field['name'] );
+			echo
+		'<ul class="accordion" data-accordion data-multi-expand="true" data-allow-all-closed="true">
+  		<li class="accordion-item" data-accordion-item>
+    	<a href="#" class="accordion-title">' . $field['label'] . '</a>
+    	<div class="accordion-content" data-tab-content>' . $value . '
+    		</div>
+  			</li>
+			</ul>';}
+			echo
+			'</div>
+			</div>';
+		
+	}
+	}}
+
 	
 	?>
 
